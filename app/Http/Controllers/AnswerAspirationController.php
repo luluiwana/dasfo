0e3fcdf22d5e00966fa9c94f2793bcd75fbc7a55
Hafizh <hafizh.arrozaq@gmail.com>
2022-10-22 00:01:15 +0700

    change bit

--- app/Http/Controllers/AnswerAspirationController.php
+++ app/Http/Controllers/AnswerAspirationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\AnswerAspiration;
+use App\Models\Aspiration;
 use Illuminate\Http\Request;
 
 class AnswerAspirationController extends Controller
@@ -14,7 +15,6 @@ class AnswerAspirationController extends Controller
      */
     public function index()
     {
-        //
     }
 
     /**
